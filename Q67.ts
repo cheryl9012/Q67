0679f4fa9671be548e8a692a2ee6948fc8f57b3f
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-04-23 23:19:33 +0500

    Add files via upload

---
+++ Q67.ts
@@ -0,0 +1,7 @@
+// Arithmetic with Mixed Types: Write a function that takes two parameters: a number and a string that represents a number (e.g., "5"). Return their sum as a number.
+
+function addNumberAndString ( number1 : number , NumberString : string) : number {
+    return number1 + Number(NumberString)
+}
+
+console.log(addNumberAndString (10 ,"5"));
